2bd97c7953f4f0599033c2f3eba5b2c3bd743e83
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2023-01-19 13:35:30 +0700

    add p2 reticle and fixing movement

--- Assets/Scripts/Minigames/Shooting/Reticle.cs
+++ Assets/Scripts/Minigames/Shooting/Reticle.cs
@@ -5,26 +5,49 @@ using UnityEngine;
 public class Reticle : MonoBehaviour
 {
     public float speed;
+    public bool isP1;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (isP1)
         {
-            gameObject.transform.position -= new Vector3(speed, 0f, 0f);
+            if (Input.GetKey(KeyCode.A))
+            {
+                gameObject.transform.position -= new Vector3(speed, 0f, 0f);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                gameObject.transform.position += new Vector3(speed, 0f, 0f);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                gameObject.transform.position += new Vector3(0f, speed, 0f);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                gameObject.transform.position -= new Vector3(0f, speed, 0f);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        else
         {
-            gameObject.transform.position += new Vector3(speed, 0f, 0f);
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            gameObject.transform.position += new Vector3(0f,speed, 0f);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            gameObject.transform.position -= new Vector3(0f, speed, 0f);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                gameObject.transform.position -= new Vector3(speed, 0f, 0f);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                gameObject.transform.position += new Vector3(speed, 0f, 0f);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                gameObject.transform.position += new Vector3(0f, speed, 0f);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                gameObject.transform.position -= new Vector3(0f, speed, 0f);
+            }
         }
     }
 }
