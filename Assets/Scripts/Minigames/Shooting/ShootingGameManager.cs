76b21d11d89de1988c5601c2e4907021f320c05f
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2023-02-11 18:29:02 +0700

    add ui to shooting minigame

--- Assets/Scripts/Minigames/Shooting/ShootingGameManager.cs
+++ Assets/Scripts/Minigames/Shooting/ShootingGameManager.cs
@@ -9,6 +9,17 @@ public class ShootingGameManager : MonoBehaviour
     public float gameTimer;
     public float timeDuration;
     public bool gameStart;
+    public bool endGame;
+
+    public FMOD.Studio.EventInstance bgm;
+
+    public Reticle p1;
+    public Reticle p2;
+
+    public GameObject HowToPlayPanel;
+    public GameObject p1EndPanel;
+    public GameObject p2EndPanel;
+    public GameObject drawEndPanel;
 
     private void Awake()
     {
@@ -26,17 +37,52 @@ public class ShootingGameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameStart = true;
+        gameStart = false;
         gameTimer = timeDuration;
+        HowToPlayPanel.SetActive(true);
+        p1EndPanel.SetActive(false);
+        p2EndPanel.SetActive(false);
+        drawEndPanel.SetActive(false);
+        bgm = FMODUnity.RuntimeManager.CreateInstance("event:/ShootingGame/BGM_ShootingGame");
+        bgm.start();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameTimer -= 1 * Time.deltaTime;
-        if(gameTimer <= 0)
+        if (Input.anyKey)
         {
-            gameStart = false;
+            gameStart = true;
+            HowToPlayPanel.SetActive(false);
+        }
+        if (gameStart)
+        {
+            gameTimer -= 1 * Time.deltaTime;
+            if (gameTimer <= 0)
+            {
+                gameStart = false;
+                endGame = true;
+            }
+        }
+        if (endGame)
+        {
+            bgm.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            if(p1.totalPoint > p2.totalPoint)
+            {
+                p1EndPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else if(p2.totalPoint > p1.totalPoint)
+            {
+                p2EndPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else if(p1.totalPoint == p2.totalPoint)
+            {
+                drawEndPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
         }
     }
 }
