c3390908b18653d447678b632737a46ddf94481a
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2023-02-11 13:06:26 +0700

    fixing all of choosing stage scene problem

--- Assets/Scripts/Gameplay/GameplayManager.cs
+++ Assets/Scripts/Gameplay/GameplayManager.cs
@@ -7,18 +7,41 @@ using FMODUnity;
 
 public class GameplayManager : MonoBehaviour
 {
+    public static GameplayManager Instance { get; private set; }
+
 
     public TextMeshProUGUI p1ScoreText;
     public TextMeshProUGUI p2ScoreText;
+
+
+    public FMOD.Studio.EventInstance bgm;
 
     public GameObject p1WinsPanel;
     public GameObject p2WinsPanel;
 
     public ScoreOverallSO overallSO;
+
+    public GameObject dialogue;
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        RuntimeManager.PlayOneShot("event:/BGM_Fairgrounds", GetComponent<Transform>().position);
+        bgm = RuntimeManager.CreateInstance("event:/BGM_Fairgrounds");
+        bgm.start();
+        dialogue.SetActive(true);
     }
 
     // Update is called once per frame
@@ -27,6 +50,10 @@ public class GameplayManager : MonoBehaviour
         p1ScoreText.text = overallSO.p1Score.ToString();
         p2ScoreText.text = overallSO.p2Score.ToString();
 
+        if(overallSO.p1Score+overallSO.p2Score > 0)
+        {
+            dialogue.SetActive(false);
+        }
         if(overallSO.p1Score + overallSO.p2Score == 5)
         {
             if(overallSO.p1Score > overallSO.p2Score)
@@ -37,8 +64,20 @@ public class GameplayManager : MonoBehaviour
             {
                 p2WinsPanel.SetActive(true);
             }
+            overallSO.switchedOn = false;
+            overallSO.playCount = 0;
+            overallSO.p1Score = 0;
+            overallSO.p2Score = 0;
             SceneManager.LoadScene("mainmenu");
+            bgm.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         }
         
+    }
+    private void OnApplicationQuit()
+    {
+        overallSO.p1Score = 0;
+        overallSO.p2Score = 0;
+        overallSO.switchedOn = false;
+        overallSO.playCount = 0;
     }
 }
--- Assets/Scripts/ScriptableObjects/ScoreOverallSO.cs
+++ Assets/Scripts/ScriptableObjects/ScoreOverallSO.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Score Overall", menuName = "Score Overall")]
 public class ScoreOverallSO : ScriptableObject
 {
+
+
     public int p1Score;
     public int p2Score;
-
+    public int playCount = 0;
+    public bool switchedOn = false;
 }
